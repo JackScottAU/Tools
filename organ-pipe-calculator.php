85d9fdc84d5e0172b34262cb2236265ec0171771
Jack Scott <jack@jackscott.id.au>
2016-03-01 08:59:28 +0000

    Add Google Analytics

--- organ-pipe-calculator.php
+++ organ-pipe-calculator.php
@@ -24,6 +24,17 @@
 		$rank = is_numeric($_GET['rank']) && isset($_GET['rank']) ? $_GET['rank'] : 4;
 		$pitch = is_numeric($_GET['pitch']) && isset($_GET['pitch']) ? $_GET['pitch'] : 440;
 	?>
+
+	<!-- Google Analytics -->
+	<script>
+  		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  		ga('create', 'UA-39588636-3', 'auto');
+		ga('send', 'pageview');
+	</script>
 </head>
 <body>
 <div class="container">
