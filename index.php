ddd23c41d6424e24749de92508118f7517362fbc
Jack Scott <jack@jackscott.id.au>
2016-03-02 15:07:17 +1100

    Added index page so that we don't show the terrible directory listing any more.

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jack's Web Tools</title>
+
+	<!-- BootStrap Stuff -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" />
+	
+	<!-- Google Adsense -->
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+	<!-- Google Analytics -->
+	<script>
+  		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  		ga('create', 'UA-39588636-3', 'auto');
+		ga('send', 'pageview');
+	</script>
+</head>
+<body>
+<div class="container">
+	<h1 style="text-align: center;">Jack's Web Tools</h1>
+	
+	<!-- Banner Ad Unit -->
+	<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9448968331199066" data-ad-slot="4253773439" data-ad-format="auto"></ins>
+	<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+	
+	<hr />
+	
+	<ul>
+		<li><a href="./organ-pipe-calculator.php">Organ Pipe Calculator</a></li>
+	</ul>
+	
+	<hr />
+	
+	<p>Code available on <a href="https://github.com/JackScottAU/Tools">GitHub</a>.</p>
+</div>
+</body>
+</html>
